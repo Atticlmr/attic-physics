26d5b4757d841010d1cdb0daaf38fd63ecd4afcc
attic <atticlmr2002@163.com>
2026-02-17 10:05:29 +0800

    init filefolder

---
+++ include/flux/autodiff.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// Automatic Differentiation API
+// Forward and reverse mode AD
+
+namespace flux {
+
+// TODO: Implement Tape and gradient computation
+
+} // namespace flux
---
+++ include/flux/simulation.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// High-level simulation world and scene management
+
+namespace flux {
+
+// TODO: Implement World, Scene, and Scheduler
+
+} // namespace flux
