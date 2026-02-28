25645e5ec39bc58426faf85011598070b834100a
Atticli <Atticlmr@gmail.com>
2026-02-28 21:48:14 +0800

    init

---
+++ src/math/quat.cpp
@@ -0,0 +1,7 @@
+#include "attic/math/quat.h"
+
+// 所有函数都在头文件内联实现
+// 此文件保留用于未来可能的复杂非内联函数
+
+namespace atp {
+} // namespace atp
--- src/physics/physics.cpp
+++ src/physics/physics.cpp
@@ -1,6 +1,4 @@
-#include "attic/math/vec3.h"
-
-// 所有函数都在头文件内联实现
+// Attic Physics 主库实现
 // 此文件保留用于未来可能的复杂非内联函数
 
 namespace atp {
